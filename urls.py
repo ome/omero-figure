aadb578bc2a429ada23b541631b3d120334c7631
Will Moore <will@lifesci.dundee.ac.uk>
2012-07-17 21:40:16 +0100

    Initial commit of bare-bones OMERO.web app 'weblabs'

---
+++ urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls.defaults import *
+
+from omeroweb.weblabs import views
+
+
+urlpatterns = patterns('django.views.generic.simple',
+
+    # index 'home page' of the weblabs app
+    url( r'^$', views.index, name='weblabs_index' ),
+
+)
---
+++ views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    """
+    Just a place-holder while we get started
+    """
+
+    return HttpResponse("Welcome to weblabs!")
