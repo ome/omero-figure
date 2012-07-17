f078860d74c4fa096334d59d5b033873646047ed
Will Moore <will@lifesci.dundee.ac.uk>
2012-07-17 23:14:12 +0100

    Adding max-intensity-indices funtionality to fast_image_stack

--- urls.py
+++ urls.py
@@ -12,4 +12,6 @@ urlpatterns = patterns('django.views.generic.simple',
     # view an image stack - loading all planes in hand
     url( r'^fast_image_stack/(?P<imageId>[0-9]+)/', views.fast_image_stack, name='weblabs_fast_image_stack' ),
 
+    # get a json 2D 'plane' where every pixel is the Z-index of max intensity (for given Channel)
+    url( r'^max_intensity_indices/(?P<imageId>[0-9]+)/(?P<theC>[0-9]+)', views.max_intensity_indices, name='weblabs_max_intensity_indices' ),
 )
--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.utils import simplejson
 
 from omeroweb.decorators import login_required
 
@@ -19,3 +20,32 @@ def fast_image_stack (request, imageId, conn=None, **kwargs):
     image = conn.getObject("Image", long(imageId))
     z_indices = range(image.getSizeZ())
     return render_to_response('weblabs/image_viewers/fast_image_stack.html', {'image':image, 'z_indices':z_indices})
+
+
+@login_required()
+def max_intensity_indices (request, imageId, theC, conn=None, **kwargs):
+    """ 
+    Returns a 2D plane (same width and height as the image) where each 'pixel' value is
+    the Z-index of the max intensity.
+    """
+    
+    image = conn.getObject("Image", long(imageId))
+    w = image.getSizeX()
+    h = image.getSizeY()
+    miPlane = [[0]*w for x in xrange(h)]
+    indexPlane = [[0]*w for x in xrange(h)]
+    
+    pixels = image.getPrimaryPixels()
+    
+    c = int(theC)
+    
+    for z in range(image.getSizeZ()):
+        plane = pixels.getPlane(z, c, 0)
+        print plane
+        for x in range(w):
+            for y in range(h):
+                if plane[y][x] > miPlane[y][x]:
+                    miPlane[y][x] = plane[y][x]
+                    indexPlane[y][x] = z
+
+    return HttpResponse(simplejson.dumps(indexPlane), mimetype='application/javascript')
