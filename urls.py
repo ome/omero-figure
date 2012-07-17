f6b239a0529310226c70307a4fd13ee43e75d668
Will Moore <will@lifesci.dundee.ac.uk>
2012-07-17 22:38:53 +0100

    fast_image_stack viewer ported from webtest branch

--- urls.py
+++ urls.py
@@ -8,4 +8,8 @@ urlpatterns = patterns('django.views.generic.simple',
     # index 'home page' of the weblabs app
     url( r'^$', views.index, name='weblabs_index' ),
 
+
+    # view an image stack - loading all planes in hand
+    url( r'^fast_image_stack/(?P<imageId>[0-9]+)/', views.fast_image_stack, name='weblabs_fast_image_stack' ),
+
 )
--- views.py
+++ views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+from omeroweb.decorators import login_required
 
 
 def index(request):
@@ -7,3 +10,12 @@ def index(request):
     """
 
     return HttpResponse("Welcome to weblabs!")
+
+
+@login_required()
+def fast_image_stack (request, imageId, conn=None, **kwargs):
+    """ Load all the planes of image into viewer, so we have them all in hand for fast viewing of stack """
+    
+    image = conn.getObject("Image", long(imageId))
+    z_indices = range(image.getSizeZ())
+    return render_to_response('weblabs/image_viewers/fast_image_stack.html', {'image':image, 'z_indices':z_indices})
