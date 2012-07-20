ca783795f0d06a1fd732882f1d7bc8611a81e52a
Will Moore <will@lifesci.dundee.ac.uk>
2012-07-20 22:05:33 +0100

    Removed mistake code added to __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-from omeroweb.weblabs import views
-
-urlpatterns = patterns('django.views.generic.simple',
-
-    # index 'home page' of the weblabs app
-    url( r'^$', views.index, name='weblabs_index' ),
-
-)
