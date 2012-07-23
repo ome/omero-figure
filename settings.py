62bcb953d5b07a7f2a1ae508c4ff1d08c1263cf7
Will Moore <will@lifesci.dundee.ac.uk>
2012-07-23 10:19:05 +0100

    weblabs rotation_3d projection plugin added via settings.py and init via jQuery plugin

---
+++ settings.py
@@ -0,0 +1,3 @@
+
+# These will override or extend corresponding global() settings in omeroweb.settings.py
+GLOBAL_APP_SETTINGS = {'omero.web.ui.right_plugins': '[["3D", "weblabs/webclient_plugins/right_plugin.rotation_3d.js.html", "rotation_3d_tab"]]'}
