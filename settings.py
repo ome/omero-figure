a1e254141a53145a949b61bfbf9ffbeebd478c57
Will Moore <will@lifesci.dundee.ac.uk>
2012-08-03 12:12:43 +0100

    Simplified settings. settings.py directly sets RIGHT_PLUGINS.
    See https://github.com/will-moore/openmicroscopy/commits/web_app_settings_8164

--- settings.py
+++ settings.py
@@ -1,3 +1,7 @@
 
-# These will override or extend corresponding global() settings in omeroweb.settings.py
-GLOBAL_APP_SETTINGS = {'omero.web.ui.right_plugins': '[["3D", "weblabs/webclient_plugins/right_plugin.rotation_3d.js.html", "rotation_3d_tab"]]'}
+# This settings.py file will be imported by omero.settings file AFTER it has initialised custom settings.
+from django.conf import settings
+
+# We can directly manipulate the settings
+# E.g. add plugins to RIGHT_PLUGINS list
+settings.RIGHT_PLUGINS.append(["3D", "weblabs/webclient_plugins/right_plugin.rotation_3d.js.html", "rotation_3d_tab"])
