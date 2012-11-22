ebb726d0f2bb7a61a1e4ef27821ed945a7518cc1
Will Moore <will@lifesci.dundee.ac.uk>
2012-11-22 00:03:34 +0000

    ROI table shows shapes too

--- static/weblabs/js/raphael-backbone/table-views.js
+++ static/weblabs/js/raphael-backbone/table-views.js
@@ -23,7 +23,7 @@ var RoiTableViewManager = Backbone.View.extend({
 
 var RoiTableView = Backbone.View.extend({
     
-    tagName: "tr",
+    tagName: "tbody",
 
     //template: _.template($('#item-template').html()),
     
@@ -31,10 +31,54 @@ var RoiTableView = Backbone.View.extend({
     
     initialize: function() {
         this.model.on('change', this.render, this);
+        
+        this.$el = $("<tr></tr>").appendTo(this.$el);
+        
+        var self = this;
+        // Add Views for any existing shapes models
+        this.model.shapes.each(function(shape) {
+            self.create_shape_view(shape);
+        });
+        
+        // If a shape is added, Create View for that too
+        this.model.shapes.on("add", function(shape) {
+            self.create_shape_view(shape);
+        });
     },
     
     render: function() {
-      this.$el.html("ROI " + this.model.get('id'));
+      this.$el.html("<td>ROI " + this.model.get('id') + "</td>");
       return this;
     },
+    
+    create_shape_view: function(shape) {
+        
+        var view = new TableShapeView({model:shape});
+        console.log( view.render().el, this.$el, this.$el.parent());
+        this.$el.parent().append(view.render().el);
+    }
+});
+
+
+
+var TableShapeView = Backbone.View.extend({
+    tagName: "tr",
+    
+    initialize: function() {
+        this.model.on('change', this.render, this);
+    },
+    
+    render: function() {
+        var text = "Shape " + this.model.get('id') + " " + this.model.get('type') + " theZ: " + this.model.get('theZ');
+        if (this.model.get('x')) {
+            text += " x:" + this.model.get('x');
+            text += " y:" + this.model.get('y');
+        }
+        if (this.model.get('cx')) {
+            text += " cx:" + this.model.get('cx');
+            text += " cy:" + this.model.get('cy');
+        }
+      this.$el.html(text);
+      return this;
+    }
 });
