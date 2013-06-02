c8ddb6a3dacc24b318121cb6fe9a2b896d2c096b
Will Moore <will@lifesci.dundee.ac.uk>
2013-06-03 00:29:49 +0100

    Rework and tidy selection handling

--- static/weblabs/js/figureshop/figureshop.js
+++ static/weblabs/js/figureshop/figureshop.js
@@ -1,8 +1,5 @@
 
 
-
-
-
     // ----------------------- Backbone MODEL --------------------------------------------
 
     // ------------------------ Panel -----------------------------------------
@@ -12,12 +9,15 @@
 
         defaults: {
             x: 100,     // coordinates on the 'paper'
-            y: 100
+            y: 100,
+            width: 512,
+            height: 512
         },
 
         initialize: function() {
         },
 
+        // When the canvas is clicked etc, we test if it's on a Panel
         containsPoint: function(x, y) {
             var panel_x = this.get('x'),
                 panel_y = this.get('y');
@@ -28,9 +28,6 @@
             return false;
         },
 
-        notifyClicked: function(event) {
-            this.trigger('click', [event]);
-        }
     });
 
     // ------------------------ Panel Collection -------------------------
@@ -52,19 +49,6 @@
             //});
         },
 
-        handleClick: function(event, x, y) {
-
-            // Check if any panel was clicked
-            var panel_clicked = false;
-            this.panels.each(function(p) {
-                console.log(p, p.containsPoint(x, y));
-                if (!panel_clicked && p.containsPoint(x, y)) {
-                    p.notifyClicked(event);
-                    panel_clicked = true
-                }
-            });
-            return panel_clicked;
-        }
     });
 
 
@@ -74,6 +58,7 @@
     // E.g. Zoom, selected panels etc.
     // Every PanelView has reference to a single UiState to notify
     // of clicks etc.
+    // UiState knows about the FigureModel, to check panels for clicks etc.
     var UiState = Backbone.Model.extend({
 
         defaults: {
@@ -104,12 +89,33 @@
             return this.selectedItems;
         },
 
-        handleClick: function(event, canvas_x, canvas_y) {
-            console.log("trigger click");
-            if (!this.model.handleClick(event, canvas_x, canvas_y)) {
+
+        // Main canvas click handler
+        // x and y refer to coordinates on the paper
+        handleClick: function(event, x, y) {
+
+            // Check if any panel was clicked
+            var panel_found = false,
+                self = this;
+            this.model.panels.each(function(p) {
+                if (panel_found) return;      // only handle first one
+
+                // If clicked, set or add to selection
+                if (p.containsPoint(x, y)) {
+                    panel_found = true;
+                    if (event.shiftKey) {
+                        self.addSelected(p);
+                    } else {
+                        self.setSelected(p);
+                    }
+                }
+            });
+
+            // If no panels were clicked, clear selection
+            if (!panel_found) {
                 this.clearSelected();
             }
-        },
+        }
 
     });
 
@@ -117,33 +123,21 @@
     // -------------------------- Backbone VIEWS -----------------------------------------
 
     // -------------------------Panel View -----------------------------------
-    // A Panel is a <td>, added to the <tr> by the Figure Table view below.
+    // A Panel is a <div>, added to the #paper by the FigureView below.
     var PanelView = Backbone.View.extend({
         tagName: "div",
         className: "panel",
         template: _.template($('#figure_panel_template').html()),
 
         initialize: function(opts) {
-            this.uiState = opts.uiState;
+            // this.uiState = opts.uiState;
             // we render on Changes in the model OR selected shape etc.
             this.model.on('destroy', this.remove, this);
             this.listenTo(this.model, 'change', this.render);
-            this.listenTo(this.model, 'click', this.handleClick);
-            //this.template = _.template($('#table_shape-template').html());
         },
 
         events: {
             // "click .img_panel": "select_panel"
-        },
-
-        handleClick: function(data) {
-            console.log("PanelView click");
-            var event = data[0];
-            if (event.shiftKey) {
-                this.uiState.addSelected(this.model);
-            } else {
-                this.uiState.setSelected(this.model);
-            }
         },
 
         render: function() {
@@ -177,43 +171,51 @@
             this.paper = Raphael("canvas_wrapper", this.orig_width, this.orig_height);
 
             // Add global click handler
-            $("#canvas_wrapper>svg").click(function(event){
-                var zoom_fraction = self.model.get('curr_zoom') * 0.01,
-                    ost = $("#canvas_wrapper").offset(),
-                    click_x = ((event.pageX - ost.left)/zoom_fraction >> 0) - self.paper_left,
-                    click_y = (event.pageY - ost.top)/zoom_fraction >> 0;
-                click_y = click_y - self.paper_top;
-
+            $("#canvas_wrapper>svg").mousedown(function(event){
                 self.handleClick(event, click_x, click_y);
             });
 
             // respond to uiStatus changes
             this.listenTo(this.model, 'change:curr_zoom', this.setZoom);
-            this.listenTo(this.model, 'change:seletion', this.setSelection);
+            this.listenTo(this.model, 'change:seletion', this.render);
         },
 
+        // User has zoomed the UI - work out new sizes etc...
         setZoom: function() {
             var scale = this.model.get('curr_zoom') * 0.01,
                 newWidth = parseInt(this.orig_width * scale),
                 newHeight = parseInt(this.orig_height * scale);
             this.paper.setSize(newWidth, newHeight);
+            // re-render at the new coordinates
+            this.render();
         },
 
-        handleClick: function(event, canvas_x, canvas_y) {
+        // Canvas mouse-down. Need to work out x and y on paper at current zoom
+        handleClick: function(event) {
+            var zoom_fraction = self.model.get('curr_zoom') * 0.01,
+                    ost = $("#canvas_wrapper").offset(),
+                    click_x = ((event.pageX - ost.left)/zoom_fraction >> 0) - self.paper_left,
+                    click_y = ((event.pageY - ost.top)/zoom_fraction >> 0) - self.paper_top;
+            // Let the model work out what to do with it (check panels etc)
             this.model.handleClick(event, canvas_x, canvas_y);
         },
 
-        setSelection: function() {
+        // draw outlines around any selected panels
+        render: function() {
             this.paper.clear();
             var zoom = this.model.get('curr_zoom') * 0.01,
                 self = this;
             this.model.getSelected().each(function(m){
-                var rect_x = (self.paper_left + m.get('x')) * zoom,
-                    rect_y = (self.paper_top + m.get('y')) * zoom,
-                    rect_w = 103,
-                    rect_h = 103;
-                console.log( rect_x, rect_y );
-                self.paper.rect(rect_x, rect_y, rect_w, rect_h).attr('stroke', '#ff0000');
+                var rect_x = (self.paper_left + 1 + m.get('x')) * zoom,
+                    rect_y = (self.paper_top + 1 + m.get('y')) * zoom,
+                    rect_w = m.get('width') * zoom,
+                    rect_h = m.get('height') * zoom,
+                    path1 = "M" + rect_x +","+ rect_y +"l"+ rect_w +","+ rect_h,
+                    path2 = "M" + (rect_x+rect_w) +","+ rect_y +"l-"+ rect_w +","+ rect_h;
+                // rectangle plus 2 diagonal lines
+                self.paper.rect(rect_x, rect_y, rect_w, rect_h).attr('stroke', '#4b80f9');
+                self.paper.path(path1).attr('stroke', '#4b80f9');
+                self.paper.path(path2).attr('stroke', '#4b80f9');
             });
         }
     })
@@ -224,39 +226,31 @@
 
 
         initialize: function(opts) {
-            this.uiState = opts.uiState;
+            // this.uiState = opts.uiState;
             var self = this;
 
             // Render on changes to the model
             this.model.on('change', this.render, this);
 
-            // The element that is 'rendered' for the ROI is the <tr>
-            // this.$el = $("<tr></tr>").appendTo(this.$el);
             this.$el = $("#paper");
 
-            // If a panel is added, need to re-render whole table
+            // If a panel is added...
             this.model.panels.on("add", this.addOne, this);
 
+            // 'Auto-render' on init.
             this.render();
         },
 
+        // Add a panel to the view
         addOne: function(panel) {
-            var view = new PanelView({model:panel, uiState:this.uiState});
+            var view = new PanelView({model:panel});    // uiState:this.uiState
             this.$el.append(view.render().el);
         },
 
-        // Render is called on init() and when we add panels.
+        // Render is called on init(). TODO - Move more main UI code here.
         render: function() {
             var self = this;
 
-            // clean...
-            this.$el.empty();
-
-            // Go through all the Figure panels...
-            // this.model.panels.each(function(panel, i) {
-            //     var view = new PanelView({model:panel, uiState:self.uiState});
-            //     self.$el.append(view.render().el);
-            // });
             return this;
         }
     });
