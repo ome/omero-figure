88d233b75b19305f82e7ba8bf6a4475a921f9923
Will Moore <w.moore@dundee.ac.uk>
2014-05-01 23:17:45 +0100

    Fixing jasmine test

--- spec/PanelSpec.js
+++ spec/PanelSpec.js
@@ -3,6 +3,6 @@ describe("Panel", function() {
     it("should have default zoom of 100%", function(){
 
         var p = new Panel();
-        expect(p.get('zoom')).toBe(0);
+        expect(p.get('zoom')).toBe(100);
     });
 })
