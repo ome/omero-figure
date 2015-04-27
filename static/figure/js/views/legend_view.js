86867219811cb51070b15338ae430e5dcff2e79c
Will Moore <w.moore@dundee.ac.uk>
2015-04-27 15:12:13 +0100

    Click legend to expand/edit/open link new tab

--- static/figure/js/views/legend_view.js
+++ static/figure/js/views/legend_view.js
@@ -39,6 +39,25 @@ var LegendView = Backbone.View.extend({
             "click .collapse-legend": "collapseLegend",
             "click .expand-legend": "expandLegend",
             "click .markdown-info": "markdownInfo",
+            "click .panel-body p": "legendClick",
+        },
+
+        // Click on the legend <p>. Start editing or follow link
+        legendClick: function(event) {
+            event.preventDefault();
+            // If link, open new window / tab
+            if (event.target.nodeName.toLowerCase() == "a") {
+                var href = event.target.getAttribute('href');
+                window.open(href, '_blank');
+                return false;
+            // Click on legend text expands then edits legend
+            } else {
+                if (this.model.get("legend_collapsed")) {
+                    this.expandLegend();
+                } else {
+                    this.editLegend();
+                }
+            }
         },
 
         markdownInfo: function(event) {
@@ -67,7 +86,7 @@ var LegendView = Backbone.View.extend({
         },
 
         editLegend: function(event) {
-            event.preventDefault();
+            if (event) event.preventDefault();
             this.editing = true;
             if (this.model.get("legend_collapsed")) {
                 this.model.set("legend_collapsed", false);
@@ -121,7 +140,7 @@ var LegendView = Backbone.View.extend({
 
                 $panel.addClass('editing');
                 $panel.show();
-                var html = '<textarea class="form-control" rows="7" style="resize:none">'
+                var html = '<textarea class="form-control" rows="9" style="resize:none">'
                             + legendText + '</textarea>';
                 $legend.html(html);
             } else {
