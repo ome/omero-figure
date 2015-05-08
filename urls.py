6f76a38889baada8ddc54ef6d13370afa1130165
Will Moore <w.moore@dundee.ac.uk>
2015-05-08 22:54:43 +0100

    shape_editor/ url

--- urls.py
+++ urls.py
@@ -24,33 +24,37 @@ else:
 
 from figure import views
 
-urlpatterns = patterns('django.views.generic.simple',
+urlpatterns = patterns(
+
+    'django.views.generic.simple',
 
     # index 'home page' of the figure app
-    url( r'^$', views.index, name='figure_index' ),
-    url( r'^new/$', views.index, name='new_figure' ),
-    url( r'^file/(?P<fileId>[0-9]+)/$', views.index, name='load_figure' ),
+    url(r'^$', views.index, name='figure_index'),
+    url(r'^new/$', views.index, name='new_figure'),
+    url(r'^file/(?P<fileId>[0-9]+)/$', views.index, name='load_figure'),
 
-    url( r'^imgData/(?P<imageId>[0-9]+)/$', views.imgData_json, name='figure_imgData'),
+    url(r'^shape_editor/$', views.shape_editor, name='shape_editor'),
+
+    url(r'^imgData/(?P<imageId>[0-9]+)/$', views.imgData_json, name='figure_imgData'),
 
     # Send json to OMERO to create pdf using scripting service
-    url( r'^make_web_figure/', views.make_web_figure, name='make_web_figure'),
+    url(r'^make_web_figure/', views.make_web_figure, name='make_web_figure'),
 
     # Save json to file annotation
-    url( r'^save_web_figure/', views.save_web_figure, name='save_web_figure'),
+    url(r'^save_web_figure/', views.save_web_figure, name='save_web_figure'),
 
     # Get json from file (file annotation Id)
-    url( r'^load_web_figure/(?P<fileId>[0-9]+)/$', views.load_web_figure, name='load_web_figure'),
+    url(r'^load_web_figure/(?P<fileId>[0-9]+)/$', views.load_web_figure, name='load_web_figure'),
 
     # List file annotations of saved Figures
-    url( r'^list_web_figures/', views.list_web_figures, name='list_web_figures'),
+    url(r'^list_web_figures/', views.list_web_figures, name='list_web_figures'),
 
     # Delete file annotations of saved Figures - 'POST' with 'fileId' of file annotation
-    url( r'^delete_web_figure/$', views.delete_web_figure, name='delete_web_figure'),
+    url(r'^delete_web_figure/$', views.delete_web_figure, name='delete_web_figure'),
 
     # Converts Lengths of value in 'fromUnit' to 'toUnit'.
     # E.g. unit_conversion/1.12/MICROMETER/ANGSTROM/.
     # Returns result as json with keys of 'value', 'unit' and 'symbol'
-    url( r'^unit_conversion/(?P<value>[0-9.]+)/(?P<fromUnit>[A-Z]+)/(?P<toUnit>[A-Z]+)/$',
-            views.unit_conversion, name='unit_conversion'),
+    url(r'^unit_conversion/(?P<value>[0-9.]+)/(?P<fromUnit>[A-Z]+)/(?P<toUnit>[A-Z]+)/$',
+        views.unit_conversion, name='unit_conversion'),
 )
