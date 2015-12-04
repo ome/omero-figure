ee4f33161d845a7f613d65637213b296dc0e1f22
Will Moore <w.moore@dundee.ac.uk>
2015-12-04 15:41:16 +0000

    Flake8 fixes

--- settings.py
+++ settings.py
@@ -1,7 +1,9 @@
 
 # This settings.py file will be imported by omero.settings file AFTER it has
 # initialised custom settings.
-# See https://www.openmicroscopy.org/site/support/omero5.2/developers/Web/CreateApp.html#app-settings
+# See "App Settings" on
+# https://www.openmicroscopy.org/
+#   site/support/omero5.2/developers/Web/CreateApp.html
 
 
 def identity(x):
@@ -10,5 +12,6 @@ def identity(x):
 
 # import json
 CUSTOM_SETTINGS_MAPPINGS = {
-    "omero.web.figure.version": ["OMERO_FIGURE_VERSION", "1.2.0", identity, None]
+    "omero.web.figure.version":
+    ["OMERO_FIGURE_VERSION", "1.2.0", identity, None]
 }
