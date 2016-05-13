8b01b9a1209ac42e3eae410fe83d5937d2c05f13
William Moore <w.moore@dundee.ac.uk>
2016-05-13 10:29:03 +0100

    Use hard-coded version 1.2.2-dev instead of settings.py
    CUSTOM_SETTINGS_MAPPINGS is parsed differently in 5.0.x making it hard to
    be compatible with OMERO 5.0.x and 5.1.0.
    Since we don't need to allow users to set the version number this can
    be hard-coded.

--- settings.py
+++ settings.py
@@ -4,14 +4,3 @@
 # See "App Settings" on
 # https://www.openmicroscopy.org/
 #   site/support/omero5.2/developers/Web/CreateApp.html
-
-
-def identity(x):
-    return x
-
-
-# import json
-CUSTOM_SETTINGS_MAPPINGS = {
-    "omero.web.figure.version":
-    ["OMERO_FIGURE_VERSION", "1.2.1", identity, None]
-}
