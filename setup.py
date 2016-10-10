47ff2cc1f68f68782628128c8a15fce56b3078cd
Jean-Marie Burel <j.burel@dundee.ac.uk>
2016-10-10 15:45:10 +0100

    Read values from package.json

--- setup.py
+++ setup.py
@@ -37,17 +37,22 @@ def read_file(fname, content_type=None):
             data = f.read()
     return data
 
-VERSION = read_file('package.json', content_type='json')['version']
+data = read_file('package.json', content_type='json')
+VERSION = data['version']
+DESCRIPTION = data['description']
+AUTHOR = data['author']
+LICENSE = data['license']
+HOMEPAGE = data['homepage']
 
 setup(name="omero-figure",
       packages=find_packages(exclude=['ez_setup']),
       version=VERSION,
-      description="A Python plugin for OMERO.web",
+      description=DESCRIPTION,
       long_description=read_file('README.rst'),
-      author='The Open Microscopy Team',
+      author=AUTHOR,
       author_email='ome-devel@lists.openmicroscopy.org.uk',
-      license='AGPLv3',
-      url="https://github.com/ome/omero-figure",
+      license=LICENSE,
+      url=HOMEPAGE,
       download_url='https://github.com/ome/omero-figure/tarball/%s' % VERSION,
       keywords=['OMERO.web', 'figure'],
       install_requires=[],
