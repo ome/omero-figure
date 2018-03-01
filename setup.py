9b7b127af3aa38f0ddb35fcb7df5e3182f3edef3
William Moore <w.moore@dundee.ac.uk>
2018-03-01 21:28:29 +0000

    Roll back adding grunt copy:shapeEditor to setup.py

--- setup.py
+++ setup.py
@@ -139,7 +139,6 @@ class Grunt(Command):
         self.spawn(['grunt', 'jst'])
         self.spawn(['grunt', 'concat'])
         self.spawn(['grunt', 'jshint', '--force'])
-        self.spawn(['grunt', 'copy:shapeEditor'])
 
 
 cmdclass['grunt'] = Grunt
