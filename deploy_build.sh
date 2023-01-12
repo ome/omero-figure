dfdaa95cfaddca6961d9e6901478cb1436144293
William Moore <w.moore@dundee.ac.uk>
2023-01-12 10:50:30 +0000

    deploy_build.sh includes mkdir templates/omero_figure

--- deploy_build.sh
+++ deploy_build.sh
@@ -8,4 +8,5 @@ cp node_modules/bootstrap-icons/font/bootstrap-icons.css omero_figure/static/ome
 cp node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff2 omero_figure/static/omero_figure/fonts/
 
 # output dir is static dir (js & css in correct place) - only need to move index.html
+mkdir -p omero_figure/templates/omero_figure/
 cp omero_figure/static/omero_figure/index.html omero_figure/templates/omero_figure/
