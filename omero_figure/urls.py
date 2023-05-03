33cc7b7682e02f2d492b7a9363b0eb83ccab9adb
William Moore <w.moore@dundee.ac.uk>
2023-05-03 11:08:39 +0100

    urls.py uses re_path

--- omero_figure/urls.py
+++ omero_figure/urls.py
@@ -18,75 +18,75 @@
 
 from omeroweb.webgateway import views as webgateway_views
 from . import views
-from django.conf.urls import url
+from django.urls import re_path
 
 
 urlpatterns = [
 
     # index 'home page' of the figure app
-    url(r'^$', views.index, name='figure_index'),
-    url(r'^new/$', views.index, name='new_figure'),
-    url(r'^recover/$', views.index, name='recover_figure'),
-    url(r'^open/$', views.index, name='open_figure'),
-    url(r'^file/(?P<file_id>[0-9]+)/$', views.index, name='load_figure'),
+    re_path(r'^$', views.index, name='figure_index'),
+    re_path(r'^new/$', views.index, name='new_figure'),
+    re_path(r'^recover/$', views.index, name='recover_figure'),
+    re_path(r'^open/$', views.index, name='open_figure'),
+    re_path(r'^file/(?P<file_id>[0-9]+)/$', views.index, name='load_figure'),
 
-    url(r'^imgData/(?P<image_id>[0-9]+)/$', views.img_data_json,
+    re_path(r'^imgData/(?P<image_id>[0-9]+)/$', views.img_data_json,
         name='figure_imgData'),
 
     # Send json to OMERO to create pdf using scripting service
-    url(r'^make_web_figure/', views.make_web_figure, name='make_web_figure'),
+    re_path(r'^make_web_figure/', views.make_web_figure, name='make_web_figure'),
 
     # Save json to file annotation
-    url(r'^save_web_figure/', views.save_web_figure, name='save_web_figure'),
+    re_path(r'^save_web_figure/', views.save_web_figure, name='save_web_figure'),
 
     # Get json from file (file annotation Id)
-    url(r'^load_web_figure/(?P<file_id>[0-9]+)/$', views.load_web_figure,
+    re_path(r'^load_web_figure/(?P<file_id>[0-9]+)/$', views.load_web_figure,
         name='load_web_figure'),
 
     # List file annotations of saved Figures
-    url(r'^list_web_figures/', views.list_web_figures,
+    re_path(r'^list_web_figures/', views.list_web_figures,
         name='list_web_figures'),
 
-    url(r'^render_thumbnail/(?P<iid>[0-9]+)/$',
+    re_path(r'^render_thumbnail/(?P<iid>[0-9]+)/$',
         webgateway_views.render_thumbnail,
         {'_defcb': views.default_thumbnail},
         name="figure_render_thumbnail"),
 
     # Region defined by ?region=x,y,w,h
-    url(r'^render_scaled_region/(?P<iid>[0-9]+)/(?P<z>[0-9]+)/(?P<t>[0-9]+)/$',
+    re_path(r'^render_scaled_region/(?P<iid>[0-9]+)/(?P<z>[0-9]+)/(?P<t>[0-9]+)/$',
         views.render_scaled_region,
         name="figure_render_scaled_region"),
 
     # Delete file annotations of saved Figures - 'POST' with 'fileId' of file
     # annotation
-    url(r'^delete_web_figure/$', views.delete_web_figure,
+    re_path(r'^delete_web_figure/$', views.delete_web_figure,
         name='delete_web_figure'),
 
     # Converts Lengths of value in 'fromUnit' to 'toUnit'.
     # E.g. unit_conversion/1.12/MICROMETER/ANGSTROM/.
     # Returns result as json with keys of 'value', 'unit' and 'symbol'
-    url(r'^unit_conversion/(?P<value>[0-9.]+)/(?P<from_unit>[A-Z]+)/'
+    re_path(r'^unit_conversion/(?P<value>[0-9.]+)/(?P<from_unit>[A-Z]+)/'
         '(?P<to_unit>[A-Z]+)/$',
         views.unit_conversion, name='unit_conversion'),
 
     # Get timestamps in seconds for images
     # Use query ?image=1&image=2
-    url(r'^timestamps/$', views.timestamps, name='figure_timestamps'),
+    re_path(r'^timestamps/$', views.timestamps, name='figure_timestamps'),
 
     # Get Z scale for images
     # Use query ?image=1&image=2
-    url(r'^z_scale/$', views.z_scale, name='figure_z_scale'),
+    re_path(r'^z_scale/$', views.z_scale, name='figure_z_scale'),
 
-    url(r'^roiCount/(?P<image_id>[0-9]+)/$', views.roi_count,
+    re_path(r'^roiCount/(?P<image_id>[0-9]+)/$', views.roi_count,
         name='figure_roiCount'),
 
-    url(r'^roiRectangles/(?P<image_id>[0-9]+)/$', views.roi_rectangles,
+    re_path(r'^roiRectangles/(?P<image_id>[0-9]+)/$', views.roi_rectangles,
         name='figure_roiRectangles'),
 
     # POST to change figure to new group with ann_id and group_id
-    url(r'chgrp/$', views.chgrp, name='figure_chgrp'),
+    re_path(r'chgrp/$', views.chgrp, name='figure_chgrp'),
 
     # Get group and owner info for multiple images. ?image=1,2,3
-    url(r'images_details/', views.images_details,
+    re_path(r'images_details/', views.images_details,
         name="figure_images_details")
 ]
