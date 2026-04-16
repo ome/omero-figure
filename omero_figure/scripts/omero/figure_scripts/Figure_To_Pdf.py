22f03adc233b25c046289181f0ee1caeab5348a0
William Moore <w.moore@dundee.ac.uk>
2026-04-16 10:18:41 +0100

    Bump version: 8.0.0rc1 → 8.0.1.dev0

--- omero_figure/scripts/omero/figure_scripts/Figure_To_Pdf.py
+++ omero_figure/scripts/omero/figure_scripts/Figure_To_Pdf.py
@@ -75,7 +75,7 @@ except ImportError:
     reportlab_installed = False
     logger.error("Reportlab not installed.")
 
-VERSION = "8.0.0"
+VERSION = "8.0.1.dev0"
 DEFAULT_OFFSET = 0
 
 ORIGINAL_DIR = "1_originals"
