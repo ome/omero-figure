106445447d5c43c1d38dece1d89e09d081fae8fc
William Moore <w.moore@dundee.ac.uk>
2026-09-08 15:06:31 +0100

    Fix figure/dist location of build

--- deploy_build.sh
+++ deploy_build.sh
@@ -7,16 +7,16 @@ mkdir -p omero_figure/static/omero_figure/fonts/
 cp figure/node_modules/bootstrap-icons/font/bootstrap-icons.css omero_figure/static/omero_figure/
 cp figure/node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff2 omero_figure/static/omero_figure/fonts/
 
-# output dir is figure/build/* 
+# output dir is figure/dist/* - Need to copy into omero_figure...
 mkdir -p omero_figure/templates/omero_figure/
 
 # First copy index.html to templates...
 echo "copying index.html to templates..."
-cp figure/build/index.html omero_figure/templates/omero_figure/
+cp figure/dist/index.html omero_figure/templates/omero_figure/
 
 # Then copy static assets (js & css)
 echo "copying static assets (js & css) to static directory..."
-cp figure/build/assets/* omero_figure/static/omero_figure/
+cp figure/dist/assets/* omero_figure/static/omero_figure/
 
 # Also copy the Figure_To_Pdf.py script to omero-figure...
 mkdir -p omero_figure/scripts/omero/figure_scripts/
